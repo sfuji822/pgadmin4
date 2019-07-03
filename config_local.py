296a7655be981e9d1def1798538769421fdc565a
sfuji822 <sfujimoto822@gmail.com>
2019-07-03 21:14:31 +0900

    correct data storage path

---
+++ config_local.py
@@ -0,0 +1,5 @@
+LOG_FILE = '/var/www/pgadmin4/pgadmin4.log'
+SQLITE_PATH = '/var/www/pgadmin4/pgadmin4.db'
+SESSION_DB_PATH = '/var/www/pgadmin4/sessions'
+STORAGE_DIR = '/var/www/pgadmin4/storage'
+
